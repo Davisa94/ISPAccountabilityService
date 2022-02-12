0643d512e9a23b6d343cc5b4d5e1bfcb6ee26267
Davisa94 <Austin.i.davis94@gmail.com>
2022-02-11 22:52:04 -0800

    Add project files.

---
+++ Service1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        //Instantiate a Timer for timing pings and speed tests
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
